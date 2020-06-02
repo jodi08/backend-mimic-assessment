83927898c64dd9718fce988031813148b40a11f8
Jo Anna Mollman <joanna.mollman@gmail.com>
2020-06-02 10:18:44 -0700

    finish project

--- mimic.py
+++ mimic.py
@@ -19,32 +19,30 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "???"
+__author__ = "jodi08 aka Jo Anna Mollman"
+# with help from Kano and Ruben
 
 import random
 import sys
 
 
 def create_mimic_dict(filename):
-    """Returns a dict mapping each word to a list of words which follow it.
-    For example:
-        Input: "I am a software developer, and I don't care who knows"
-        Output:
-            {
-                "" : ["I"],
-                "I" : ["am", "don't"],
-                "am": ["a"],
-                "a": ["software"],
-                "software" : ["developer,"],
-                "developer," : ["and"],
-                "and" : ["I"],
-                "don't" : ["care"],
-                "care" : ["who"],
-                "who" : ["knows"]
-            }
-    """
-    # +++your code here+++
-    pass
+    """Returns a dict mapping each word to a list of words which follow it."""
+    with open(filename, "r") as f:  # open and read file
+        read_mimic = f.read().split()  # split on white space
+        mimic_dict = {"": [read_mimic[0]]}  # setting the starting string
+        # getting an index number for word as we loop through read file
+        for val, word in enumerate(read_mimic):
+            if val < len(read_mimic) - 1:  # checking
+                if word in mimic_dict:
+                    mimic_dict[word].append(read_mimic[val + 1])
+                elif word not in mimic_dict:
+                    mimic_dict[word] = [read_mimic[val+1]]  # + val = next word
+            # if word is key in read_mimic append word as value to key
+            # if word is not a key in read_mimic add as key and add next word as value
+    # print(mimic_dict)
+    return mimic_dict
+# need to make this print the word and the word following
 
 
 def print_mimic(mimic_dict, start_word):
@@ -55,8 +53,18 @@ def print_mimic(mimic_dict, start_word):
         - Randomly select a new word from the next-list
         - Repeat this process 200 times
     """
-    # +++your code here+++
-    pass
+    i = 0
+    random_word = start_word
+    while i < 200:
+        if start_word in mimic_dict:
+            next_word = random.choice(mimic_dict[start_word])
+            random_word += " " + next_word + " "
+            start_word = next_word
+            i += 1
+        else:
+            start_word = ""
+            i += 1
+    print(random_word)
 
 
 # Provided main(), calls mimic_dict() and print_mimic()
@@ -67,6 +75,7 @@ def main():
 
     d = create_mimic_dict(sys.argv[1])
     print_mimic(d, '')
+    return d
 
 
 if __name__ == '__main__':
